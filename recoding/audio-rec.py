dfa3fd768218553d7870f1f736796db9ff2bb95d
seoyoungnation <ourworlds19@gmail.com>
2019-11-23 01:10:09 +0900

    edit interface

--- recoding/audio-rec.py
+++ recoding/audio-rec.py
@@ -11,15 +11,18 @@ import subprocess
 
 root = tk.Tk()
 root.title('Recorder')
-photo = PhotoImage(file='microphone.png').subsample(35, 35)
 
 label1 = ttk.Label(root, text = 'File Name')
 label1.grid(row = 0, column = 0)
 
 entry1 = ttk.Entry(root, width = 40) 
-entry1.grid(row = 0, column = 1, columnspan = 4)
+entry1.grid(row = 0, column = 2, columnspan = 4)
 
-btn2 = tk.StringVar()
+label2 = ttk.Label(root, text = 'Your Text')
+label2.grid(row = 4, column = 0)
+
+entry2 = ttk.Entry(root, width = 40)
+entry2.grid(row = 4, column = 2, columnspan = 4)
 
 ##definitions here
 def callback():
@@ -35,22 +38,22 @@ def stop():
 def quit():
 	sys.exit()
 
-def save_file():
+def save_text():
 	sys.exit()
 
 ###
 
 
 MyButton1 = ttk.Button(root, text='Start', width=10, command=callback)
-MyButton1.grid(row = 0, column = 6)
+MyButton1.grid(row = 0, column = 8)
 
 MyButton2 = ttk.Button(root, text='Stop', width=10, command=stop)
-MyButton2.grid(row = 0, column = 16)
+MyButton2.grid(row = 0, column = 18)
 
-MyButton3 = ttk.Button(root, text = 'Save Audio', width=10, command=save_file)
-MyButton3.grid(row = 2,column = 4)
+MyButton3 = ttk.Button(root, text = 'Save Text', width=10, command=save_text)
+MyButton3.grid(row = 4,column = 8)
 
 MyButton4 = ttk.Button(root, text = 'Quit', width=10, command=quit)
-MyButton4.grid(row = 2, column = 14)
+MyButton4.grid(row = 4, column = 18)
 
 root.mainloop()
--- recoding/install.sh
+++ recoding/install.sh
@@ -1,4 +1,7 @@
-sudo apt-get install python-pyaudio
-sudo apt-get install libportaudio2 portaudio19-dev
-sudo apt-get install vorbis-tools
+sudo add-apt-repository universe
+sudo apt-get install -y python-pyaudio python3-pyaudio
 
+#sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+#sudo apt-get install ffmpeg libav-tools
+#sudo apt-get install vorbis-tools
+#sudo python3 -m pip install pyaudio
