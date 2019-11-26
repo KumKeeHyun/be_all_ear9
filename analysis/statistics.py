4160d19bda31f96fff0d1b9671d596a935a44fa4
Koosungyoon <55373976+Koosungyoon@users.noreply.github.com>
2019-11-26 20:10:18 +0900

    realfinal

--- analysis/statistics.py
+++ analysis/statistics.py
@@ -1,5 +1,13 @@
 #extract information using anly_node_que
 
-from find_error import anal_node_que
+from find_error import *
+
+c_word, v_word = read_sentence_file('C:/Users/SmartSystemsSoftware/Desktop/text/correct.txt', 'C:/Users/SmartSystemsSoftware/Desktop/text/voice.txt')
 
 
+c_word_list = split_sentence(c_word)
+v_word_list = split_sentence(v_word)
+
+for i in range(len(c_word_list)):
+    print('%s : %s =>' %(c_word_list[i], v_word_list[i]),end=' ')
+    q1 = anlys_node(c_word_list[i],v_word_list[i])
